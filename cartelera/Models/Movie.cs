ee65532dc94da44baae79c4d0b331fc9c51bb891
AngelRafael420 <202010085@itla.edu.do>
2024-03-25 21:12:07 -0600

    Agregar archivos de proyecto.

---
+++ cartelera/Models/Movie.cs
@@ -0,0 +1,20 @@
+﻿namespace cartelera.Models
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Year { get; set; }
+        public string Director { get; set; }
+        public string PosterUrl { get; set; } // Nueva propiedad para la URL de la carátula
+
+        public Movie(string title, string description, int year, string director, string posterUrl)
+        {
+            Title = title;
+            Description = description;
+            Year = year;
+            Director = director;
+            PosterUrl = posterUrl;
+        }
+    }
+}
